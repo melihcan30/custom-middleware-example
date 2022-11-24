53415347e98bde85c7a3de5dc8c03e88e7233350
Melihcan Serbest <canmelih.free3@gmail.com>
2022-11-24 17:34:57 +0300

    Add project files.

---
+++ Middlewares/LoggingMiddleware.cs
@@ -0,0 +1,25 @@
+﻿namespace custom_middleware_example.Middlewares
+{
+    public class LoggingMiddleware
+    {
+        readonly RequestDelegate _next;
+        public LoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        
+        public async Task InvokAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            finally 
+            {
+                string logText = $"{context.Request?.Method} {context.Request?.Path.Value} => {context.Response?.StatusCode}{Environment.NewLine}";
+                File.AppendAllText("log.txt", logText);
+                
+            }
+        }
+    }
+}
---
+++ Middlewares/LoggingMiddlewareExtension.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace custom_middleware_example.Middlewares
+{
+    public static class LoggingMiddlewareExtension
+    {
+        public static IApplicationBuilder UseLogging(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<LoggingMiddleware>();
+        }
+    }
+}
